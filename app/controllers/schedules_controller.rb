613426ec616dba01cb14bfd0ed8cd11bea3ce1eb
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-10-18 12:29:05 +0500

    fixed rubocop issue

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -24,11 +24,12 @@ class SchedulesController < ApplicationController
 
   def new_schedule_run; end
 
-  def run_hmc_program    
+  def run_hmc_program
     schedule_run = ScheduleRun.new(run_params.merge(start_time: DateTime.now))
     if schedule_run.save
       if check_valid_number_of_proposals(schedule_run) == true
-        render json: { errors: 'The number of proposals to be scheduled for this location is less than or equal to the Number of weeks in program year'}
+        render json: { errors: 'The number of proposals to be scheduled for this location is less than or equal to the
+                                                                                    Number of weeks in program year' }
       else
         hmc_program(schedule_run)
       end
@@ -96,10 +97,10 @@ class SchedulesController < ApplicationController
       prop1, prop2 = schedule.proposal.split(' and ')
       update_proposal_applied_date(prop1, date)
       update_proposal_applied_date(prop2, date)
-      return [prop1, prop2]
+      [prop1, prop2]
     else
       update_proposal_applied_date(schedule.proposal, date)
-      return [schedule.proposal]
+      [schedule.proposal]
     end
   end
 
@@ -168,8 +169,8 @@ class SchedulesController < ApplicationController
 
     half_proposals = proposals.where(assigned_size: "Half")
     full_proposals = proposals.where(assigned_size: "Full")
-    total_proposals = full_proposals.count + (half_proposals.count.to_f/2).ceil
-    if @location&.num_weeks < total_proposals
+    total_proposals = full_proposals.count + (half_proposals.count.to_f / 2).ceil
+    if @location&.num_weeks&.< total_proposals
       true
     else
       false
