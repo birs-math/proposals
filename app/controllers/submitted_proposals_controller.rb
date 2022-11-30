93f35b6095a926e35600dd20e3514b04236d5e21
Hassan Mahmood <devhassan.mahmood@gmail.com>
2022-11-30 19:31:58 +0500

    fixed env variable and added rpecs

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -278,7 +278,7 @@ class SubmittedProposalsController < ApplicationController
           dates: proposal.assigned_date
         }
     end
-    response = RestClient.post "#{ENV.fetch('WORKSHOP_API_URL', nil)}/events/proposals", {proposals: proposals}.to_json, content_type: 'application/json'
+    response = RestClient.post "#{ENV.fetch('WORKSHOPS_API_URL', nil)}/events/proposals", {proposals: proposals}.to_json, content_type: 'application/json'
   end
 
   def post_to_editflow
