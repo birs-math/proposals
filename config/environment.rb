93876d6a69bd2c4b696379a1c2c844c82e909243
paydaylight <maxat235@gmail.com>
2023-04-13 11:57:04 +0600

    Fix .env.rb not loading and provide default db host

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
-env_vars = File.join(Rails.root, 'config', '.env.rb')
+env_vars = File.join(Rails.root,'.env.rb')
 load(env_vars) if File.exists?(env_vars)
 
 # Initialize the Rails application.
