6b794f417ff552757798765d2d472c7e82faaeb3
paydaylight <maxat235@gmail.com>
2023-05-04 16:31:16 +0600

    Fix $ is not defined

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
     jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
