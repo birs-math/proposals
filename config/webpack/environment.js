12eb4154986bb8673e8d497f166cf3d4de36c0ec
paydaylight <maxat235@gmail.com>
2023-05-04 17:57:18 +0600

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jquery: 'jquery',
+    'window.$': 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
     'window.jQuery': 'jquery',
     jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
