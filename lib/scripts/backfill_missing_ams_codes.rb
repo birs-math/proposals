9d6e71829fbad057dfa67c6ce7f0f46d4390ebf2
paydaylight <maxat235@gmail.com>
2023-06-05 17:24:55 +0600

    Handle case when both ProposalAmsSubjects records have nil codes

--- lib/scripts/backfill_missing_ams_codes.rb
+++ lib/scripts/backfill_missing_ams_codes.rb
@@ -9,9 +9,15 @@ p "Found #{code_missing_count} ProposalAmsSubject's missing code"
 
 query.find_each do |proposal_ams_subject|
   p "Found #{proposal_ams_subject.summary}..."
+
+  if proposal_ams_subject.reload.code.present?
+    p "Already updated the record, skipping..."
+    next
+  end
+
   proposal = proposal_ams_subject.proposal
 
-  # find second AMS Subject
+  # Find second AMS Subject
   other_proposal_ams_subjects =
     proposal_ams_subject.proposal.proposal_ams_subjects.where.not(id: proposal_ams_subject.id)
 
@@ -20,28 +26,33 @@ query.find_each do |proposal_ams_subject|
     next
   end
 
-  other_code = other_proposal_ams_subjects.first.code
+  other_proposal_ams_subject = other_proposal_ams_subjects.first
+  other_code = other_proposal_ams_subject.code
 
   if other_code.nil?
-    # Possible to manually map those records, but skip for now instead
-    p "Warning: #{proposal.summary} has two AMS Subjects without code1 or code2, skipping..."
+    p "Warning: #{proposal.summary} has two AMS Subjects without code1 or code2"
+    proposal_ams_subject.update_attribute(:code, 'code1')
+    other_proposal_ams_subject.update_attribute(:code, 'code2')
+    p 'Updated both records'
     next
-  else
-    # Map existing code attribute to one that is missing
-    target_code = case other_code
-                  when 'code1'
-                    'code2'
-                  when 'code2'
-                    'code1'
-                  end
-
-    if target_code.nil?
-      p "Warning: #{proposal.summary} has other ProposalAmsSubject with unknown code label: #{other_code}"
-      next
-    end
-
-    proposal_ams_subject.update_attribute(:code, target_code)
-    p "Updated #{proposal_ams_subject.summary}, proposal: #{proposal.summary} with code: #{target_code}, " \
-      "other ProposalAmsSubject had code: #{other_code}"
   end
+
+  # Map existing code attribute to one that is missing
+  target_code = case other_code
+                when 'code1'
+                  'code2'
+                when 'code2'
+                  'code1'
+                else
+                  nil
+                end
+
+  if target_code.nil?
+    p "Warning: #{proposal.summary} has other ProposalAmsSubject with unknown code label: #{other_code}"
+    next
+  end
+
+  proposal_ams_subject.update_attribute(:code, target_code)
+  p "Updated #{proposal_ams_subject.summary}, proposal: #{proposal.summary} with code: #{target_code}, " \
+    "other ProposalAmsSubject had code: #{other_code}"
 end
