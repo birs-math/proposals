13bea52684644ebd369651f4f2953363b81cf202
paydaylight <maxat235@gmail.com>
2023-07-04 12:21:29 +0600

    Comment out code that breaks ci

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  Rails.application.routes.default_url_options[:host] = ENV['APPLICATION_HOST']
-
-  config.hosts << ENV['APPLICATION_HOST']
+  # Rails.application.routes.default_url_options[:host] = ENV['APPLICATION_HOST']
+  #
+  # config.hosts << ENV['APPLICATION_HOST']
 end
