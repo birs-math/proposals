ce9785840703fd4ab3de338c0286ed8b3ed4b418
paydaylight <maxat235@gmail.com>
2023-07-12 18:37:49 +0600

    Add whitespace after config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -135,6 +135,7 @@ Rails.application.configure do
     protocol: 'https',
     host: ENV['APPLICATION_HOST']
   }
+  
   ## SMTP Settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
